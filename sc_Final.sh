8d467cc0abd90ebc887a2586d7e6572da37c6596
Raghul A <109461607+Skyliner-dev@users.noreply.github.com>
2024-01-29 23:47:15 +0530

    Update sc_Final.sh

--- sc_Final.sh
+++ sc_Final.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+
 echo "Welcome to the basic calculator!"
+
 
 function simpleCalci() {
     echo "Enter an arithmetic operation or type 'quit' to quit:"
     read op
-    if [[ "$op =~ [-+]?[0-9]+ [-+]?[0-9]+$" ]]; then 
-           bc -l <<< "$op"
-    else 
-           echo 'Operation check failed!'
-    fi
+    while [[ $op != "quit" ]]; do
+       if [[ "$op =~ [-+]?[0-9]+ [-+]?[0-9]+$" ]]; then 
+              bc -l <<< "$op"
+       else 
+              echo 'Operation check failed!'
+       fi
+    done
+    echo 'Goodbye!'
 }
 simpleCalci()
