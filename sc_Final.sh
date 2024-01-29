a90db85356c8f4f0b218ca9d4fb5c143599d4dab
Raghul A <109461607+Skyliner-dev@users.noreply.github.com>
2024-01-30 00:06:25 +0530

    Update sc_Final.sh

--- sc_Final.sh
+++ sc_Final.sh
@@ -2,17 +2,20 @@
 
 echo "Welcome to the basic calculator!"
 
+#A text file which stores all logs,inputs and outputs
+touch operation_history.txt
 function simpleCalci() {
     while [[ $op != "quit" ]]; do
-           echo "Enter an arithmetic operation or type 'quit' to quit:"
+           echo "Enter an arithmetic operation or type 'quit' to quit:" | tee -a operation_history.txt
            read op
+           op | tee -a operation_history.txt
        if [[ "$op =~ [-+]?[0-9]+ [-+]?[0-9]+$" ]]; then 
-              bc -l <<< "$op"
+              bc -l <<< "$op" >> operation_history.txt
           
        else 
-              echo 'Operation check failed!'
+              echo 'Operation check failed!' | tee -a operation_history.txt
        fi
     done
-    echo 'Goodbye!'
+    echo 'Goodbye!' | tee -a operation_history.txt
 }
 simpleCalci
